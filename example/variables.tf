031311a45099f5998096999ba1cb947621756a00
Chirag Pareek <chirag.pareek@justice.gov.uk>
2026-03-30 14:43:02 +0100

    try 6

--- example/variables.tf
+++ example/variables.tf
@@ -2,3 +2,12 @@ variable "env" {
   default     = "sbox"
   description = "Environment name, e.g. test, stg, prod"
 }
+variable "builtFrom" {
+  description = "The source or origin of the build"
+  default     = "hmcts/terraform-module-azure-managed-redis"
+}
+
+variable "product" {
+  description = "The source or origin of the product"
+  default     = "core"
+}
