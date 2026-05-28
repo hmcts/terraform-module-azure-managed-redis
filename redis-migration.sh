45660f810664d63a05916c15a5302c4ebcb58c06
Nicholas Chang <nicholas.chang@justice.gov.uk>
2026-05-28 11:24:58 +0100

    Refactor pipeline

--- redis-migration.sh
+++ redis-migration.sh
@@ -1,16 +1,18 @@
-# Azure Cache for Redis (source)
-OLD_HOST="test"
-OLD_KEY="test"
+#!/bin/sh
+set -e
 
-# Azure Managed Redis (destination)
-NEW_HOST="test"
-NEW_KEY="test"
+echo "Starting Redis migration..."
+echo "Source: $OLD_HOST:6380"
+echo "Target: $NEW_HOST:10000"
 
 # Run the copy
 redis-cli -h $OLD_HOST -p 6380 --tls -a $OLD_KEY \
   --scan --pattern '*' | while read key; do
+    echo "Migrating: $key"
     redis-cli -h $OLD_HOST -p 6380 --tls -a $OLD_KEY \
       DUMP "$key" | redis-cli -h $NEW_HOST -p 10000 \
       --tls --user default -a $NEW_KEY \
-      -x RESTORE "$key" 0
+      -x RESTORE "$key" 0 REPLACE
 done
+
+echo "Migration complete!"
